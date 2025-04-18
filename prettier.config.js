bd4caa09ef67ea85181f22cb1dfe1092a591e4a6
photiary <54730417+photiary@users.noreply.github.com>
2025-04-19 00:42:26 +0900

    Prettier 패키지 추가 (#1)
    * prettier 패키지 추가
    
    * Prettier 로고 링크 추가

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    endOfLine: 'auto', // LF|CRLF 자동으로 개행문자 사용 https://prettier.io/docs/en/options.html#end-of-line
+}
+
+export default config
