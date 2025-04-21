70d9ae26d23804f3fa2a35c95b0187434a389253
photiary <54730417+photiary@users.noreply.github.com>
2025-04-21 20:50:04 +0900

    주석 추가 및 Mock 응답에 Type 추가 (#4)

--- src/features/counter/counterAPI.ts
+++ src/features/counter/counterAPI.ts
@@ -1,9 +1,18 @@
 import { api } from '@/app/api'
 
-// A mock function to mimic making an async request for data
+export interface Count {
+  data: number
+}
+
+/**
+ * 개수를 조회한다.
+ *
+ * @param amount 초기 개수
+ * @returns Count
+ */
 export const fetchCount = async (amount: number = 1) => {
   const response = await api.get('/api/count', {
     params: { amount },
   })
-  return response.data
+  return response.data as Count
 }
--- src/features/counter/counterMock.ts
+++ src/features/counter/counterMock.ts
@@ -1,10 +1,15 @@
 import AxiosMockAdapter from 'axios-mock-adapter'
+import { Count } from './counterAPI'
 
 const countMocks = (mock: AxiosMockAdapter) => {
   mock.onGet('/api/count').reply((config) => {
-    const data = config.params?.amount || 1
-    return [200, { data }]
+    const data: Count = {
+      data: config.params?.amount || 1,
+    }
+    return [200, data]
   })
+
+  // API mocks 추가
 }
 
 export default countMocks
