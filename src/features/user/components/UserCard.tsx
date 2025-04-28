99d24a4872a71627f2e5693765dac39838ec8779
funa <photiary@gmail.com>
2025-04-28 20:37:59 +0900

    사용자 카드 컴포넌트

--- src/features/user/components/UserCard.tsx
+++ src/features/user/components/UserCard.tsx
@@ -1,5 +1,34 @@
 import type { JSX } from 'react'
+import { Card, CardContent } from '@/components/ui/card'
 
 export const UserCard = (): JSX.Element => {
-  return <div>init</div>
+  return (
+    <div>
+      <Card className="w-[400px]">
+        <CardContent className="flex flex-row items-start gap-6">
+          {/* 프로필 이미지 */}
+
+          <div className="w-16">
+            <div className="h-16 w-16 rounded-full bg-black"></div>
+          </div>
+
+          {/* 사용자 정보 (세로 정렬) */}
+          <div className="flex flex-col justify-center space-y-1">
+            {/* 아이디 */}
+            <p className="text-sm font-medium">$[name]</p>
+
+            {/* 설명 */}
+            <p className="text-muted-foreground text-sm">
+              The React Framework - created and maintained by $[email]
+            </p>
+
+            {/* 가입 날짜 */}
+            <div className="text-muted-foreground flex items-center text-sm">
+              <span>Joined $[createDate]</span>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
