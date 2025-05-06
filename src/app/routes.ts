390c63917a22a52a0302dee077aae13a021e41dd
funa <photiary@gmail.com>
2025-05-06 17:28:53 +0900

    Layout 컴포넌트 추가

--- src/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from 'react-router'
-import App from '@/App.tsx'
-import { Home } from '@/routes/home.tsx'
-
-export const router = createBrowserRouter([
-  { path: '/', Component: App },
-  { path: '/home', Component: Home },
-])
---
+++ src/app/routes.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router'
+import App from '@/App.tsx'
+import { MainLayout } from '@/components/layouts/MainLayout.tsx'
+import { Home } from '@/routes/home'
+
+export const router = createBrowserRouter([
+  { path: '/', Component: App },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/home',
+        element: <Home />,
+      },
+    ],
+  },
+])
